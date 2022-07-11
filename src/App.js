04dd12fd02eab42e7b14d1dacffacf3aa246334a
Your Name <you@example.com>
2022-07-10 23:31:33 -0300

    Subindo meus arquivos em react.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import {useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from './services/firebase';
 import Login from './components/Login';
 import Loading from './components/Loading';
+import Sidebar from './components/Sidebar';
+import * as C from './styles/app';
+import Chat from './components/Chat';
+
 
 const App = () => {
 
   const [user, loading] = useAuthState(auth);
+  const [userChat, setUserChat] = useState(null);
 
   useEffect(() => {
     if(user) {
+      
       db.collection("users").doc(user.uid).set({
         email: user.email, 
         photoURL: user.photoURL,
@@ -24,14 +30,19 @@ const App = () => {
 
   return (
 
+    <C.Container>
 
+    <Sidebar setUserChat={setUserChat} userChat={userChat} />
 
- <div>
-    App
- </div>
+    <Chat userChat={userChat}/>
 
+    </C.Container>
 
   )
 }
+
+
+
+
 
 export default App;
